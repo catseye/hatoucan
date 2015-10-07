cb5188dacc00221705328bdda44a700299710fbd
Chris Pressey <cpressey@catseye.tc>
2015-10-07 22:17:47 +0100

    Honour quotes.  Passes all tests!  Tokenizes Bubble Escape!

--- test-src.sh
+++ test-src.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-petcat -w2 -- "$1" | hexdump -C > petcat.txt
+petcat -w2 -- "$1" 2>/dev/null | hexdump -C > petcat.txt
 script/hatoucan < "$1" | hexdump -C > hatoucan.txt
+diff -q petcat.txt hatoucan.txt && \
+  echo "Successfully tokenized identically to petcat!" && \
+  rm petcat.txt hatoucan.txt && \
+  exit 0
 diff -ru petcat.txt hatoucan.txt | cdiff
-rm -f petcat.txt hatoutcan.txt
